3403a24ccb407c6a4961dc7ed3f186776bdc34b9
xuebusi <xuebusi@qq.com>
2023-09-04 12:41:44 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ScrollParallaxEffect
 SwiftUI实现滚动图片视差效果
+
+### 截图
+<img src="screen.png" width="300" height:auto alt="screen.png"/>
---
+++ ScrollParallaxEffect/ScrollParallaxEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollParallaxEffectApp.swift
+//  ScrollParallaxEffect
+//
+//  Created by shiyanjun on 2023/9/4.
+//
+
+import SwiftUI
+
+@main
+struct ScrollParallaxEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
